6abf9b53b2b50bcf795ff946d0dbda42e69d577b
Wesley Carvalho <crafael.wesley@gmail.com>
2023-05-09 20:42:49 -0300

    f

--- src/Routing/Router.tsx
+++ src/Routing/Router.tsx
@@ -1,8 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Pages/Main/Main.";
-import CreateMember from "../Pages/CreateMember/CreateMember";
-import Members from "../Pages/Members/Members";
-import EditMember from "../Pages/EditMember/EditMember";
 import Authentication from "../Pages/Authentication/Login/Authentication";
 import Pontos from "../Pages/Pontos/Pontos";
 import Servidor from "../Pages/Servidor/Servidor";
@@ -13,18 +10,6 @@ export const router = createBrowserRouter([
 		path: "/",
 		element: <Main />,
 		children: [
-			{
-				path: "criar-membro",
-				element: <CreateMember />,
-			},
-			{
-				path: "editar-membro/:id",
-				element: <EditMember />,
-			},
-			{
-				path: "membros",
-				element: <Members />,
-			},
 			{
 				path: "pontos",
 				element: <Pontos />,
